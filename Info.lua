98cba6a4869e6a979f7d0327ad5dc4de93dc2cd3
Lukas Pioch <lukas@zgow.de>
2017-05-19 23:25:10 +0200

    Updated README and command notes in Info.lua

--- Info.lua
+++ Info.lua
@@ -27,37 +27,37 @@ g_PluginInfo =
 				{
 					Handler = HandleCreateCommand,
 					Permission = "itemspawner.create",
-					HelpString = "Create a new item spawner.",
+					HelpString = "Create a new item spawner at player's location.",
 				},
 				remove =
 				{
 					Handler = HandleRemoveCommand,
 					Permission = "itemspawner.remove",
-					HelpString = "Removes a item spawner.",
+					HelpString = "Removes a item spawner, will be disabled and deleted.",
 				},
 				list =
 				{
 					Handler = HandleListCommand,
 					Permission = "itemspawner.list",
-					HelpString = "List all item spawners.",
+					HelpString = "List all item spawners. Names in green are enabled.",
 				},
 				info =
 				{
 					Handler = HandleInfoCommand,
 					Permission = "itemspawner.info",
-					HelpString = "Show info to the item spawner.",
+					HelpString = "Show info to the item spawner. The position, radius, interval.",
 				},
 				enable =
 				{
 					Handler = HandleEnableCommand,
 					Permission = "itemspawner.enable",
-					HelpString = "Enable the item spawner.",
+					HelpString = "Enable the item spawner. Will then start spawning items in the radius at the specified interval.",
 				},
 				disable =
 				{
 					Handler = HandleDisableCommand,
 					Permission = "itemspawner.disable",
-					HelpString = "Disable the item spawner.",
+					HelpString = "Disable the item spawner. Stop spawning items. ",
 				},
 				change =
 				{
@@ -67,13 +67,13 @@ g_PluginInfo =
 						{
 							Handler = HandleChangeIntervalCommand,
 							Permission = "itemspawner.change.interval",
-							HelpString = "Change the interval.",
+							HelpString = "Change the interval. The time in seconds, between the spawning.",
 						},
 						radius =
 						{
 							Handler = HandleChangeRadiusCommand,
 							Permission = "itemspawner.change.radius",
-							HelpString = "Change the radius.",
+							HelpString = "Change the radius. The radius in blocks around of the spawner.",
 						},
 					},
 				},
--- README.md
+++ README.md
@@ -6,14 +6,14 @@ Create a item spawner that spawns random items inside of a specified radius.
 | Command | Permission | Description |
 | ------- | ---------- | ----------- |
 |/itemspawner |  | Shows the list of arguments.|
-|/itemspawner change interval | itemspawner.change.interval | Change the interval.|
-|/itemspawner change radius | itemspawner.change.radius | Change the radius.|
-|/itemspawner create | itemspawner.create | Create a new item spawner.|
-|/itemspawner disable | itemspawner.disable | Disable the item spawner.|
-|/itemspawner enable | itemspawner.enable | Enable the item spawner.|
-|/itemspawner info | itemspawner.info | Show info to the item spawner.|
-|/itemspawner list | itemspawner.list | List all item spawners.|
-|/itemspawner remove | itemspawner.remove | Removes a item spawner.|
+|/itemspawner change interval | itemspawner.change.interval | Change the interval. The time in seconds, between the spawning.|
+|/itemspawner change radius | itemspawner.change.radius | Change the radius. The radius in blocks around of the spawner.|
+|/itemspawner create | itemspawner.create | Create a new item spawner at player's location.|
+|/itemspawner disable | itemspawner.disable | Disable the item spawner. Stop spawning items. |
+|/itemspawner enable | itemspawner.enable | Enable the item spawner. Will then start spawning items in the radius at the specified interval.|
+|/itemspawner info | itemspawner.info | Show info to the item spawner. The position, radius, interval.|
+|/itemspawner list | itemspawner.list | List all item spawners. Names in green are enabled.|
+|/itemspawner remove | itemspawner.remove | Removes a item spawner, will be disabled and deleted.|
 
 
 
